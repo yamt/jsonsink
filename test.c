0b02cb5852df0299f40bae03206815ab9133e94f
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-03-02 21:20:15 +0900

    avoid using a global in the callback sample

--- test.c
+++ test.c
@@ -29,10 +29,16 @@
 
 #include "jsonsink.h"
 
+struct sink {
+        struct jsonsink s;
+        FILE *fp;
+};
+
 static bool
 flush(struct jsonsink *s, size_t needed)
 {
-        size_t nwritten = fwrite(s->buf, 1, s->bufpos, stdout);
+        struct sink *sink = (void *)s;
+        size_t nwritten = fwrite(s->buf, 1, s->bufpos, sink->fp);
         if (nwritten != s->bufpos) {
                 return false;
         }
@@ -43,49 +49,51 @@ flush(struct jsonsink *s, size_t needed)
 int
 main(int argc, char **argv)
 {
+        struct sink sink;
         char buf[64];
-        struct jsonsink s;
-        jsonsink_init(&s);
-        jsonsink_set_buffer(&s, buf, sizeof(buf));
-        s.flush = flush;
-        jsonsink_object_start(&s);
-        JSONSINK_ADD_LITERAL_KEY(&s, "key1");
-        jsonsink_object_start(&s);
-        JSONSINK_ADD_LITERAL_KEY(&s, "key1");
-        jsonsink_add_serialized_value(&s, "100", 3);
-        JSONSINK_ADD_LITERAL_KEY(&s, "key2");
-        jsonsink_add_serialized_value(&s, "200", 3);
-        JSONSINK_ADD_LITERAL_KEY(&s, "array1");
-        jsonsink_array_start(&s);
+        struct jsonsink *s = &sink.s;
+        jsonsink_init(s);
+        jsonsink_set_buffer(s, buf, sizeof(buf));
+        sink.fp = stdout;
+        s->flush = flush;
+        jsonsink_object_start(s);
+        JSONSINK_ADD_LITERAL_KEY(s, "key1");
+        jsonsink_object_start(s);
+        JSONSINK_ADD_LITERAL_KEY(s, "key1");
+        jsonsink_add_serialized_value(s, "100", 3);
+        JSONSINK_ADD_LITERAL_KEY(s, "key2");
+        jsonsink_add_serialized_value(s, "200", 3);
+        JSONSINK_ADD_LITERAL_KEY(s, "array1");
+        jsonsink_array_start(s);
         uint32_t i;
         for (i = 0; i < 100; i++) {
-                jsonsink_object_start(&s);
-                JSONSINK_ADD_LITERAL_KEY(&s, "version");
-                JSONSINK_ADD_LITERAL(&s, "2");
-                JSONSINK_ADD_LITERAL_KEY(&s, "id");
-                jsonsink_add_uint32(&s, i);
-                JSONSINK_ADD_LITERAL_KEY(&s, "double");
-                jsonsink_add_double(&s, -1.2345);
-                JSONSINK_ADD_LITERAL_KEY(&s, "int32");
-                jsonsink_add_int32(&s, -54321);
-                JSONSINK_ADD_LITERAL_KEY(&s, "jsonsink_add_escaped_string");
-                jsonsink_add_escaped_string(&s, JSONSINK_LITERAL("foo"));
-                JSONSINK_ADD_LITERAL_KEY(&s, "jsonsink_add_serialized_value");
-                jsonsink_add_serialized_value(&s,
+                jsonsink_object_start(s);
+                JSONSINK_ADD_LITERAL_KEY(s, "version");
+                JSONSINK_ADD_LITERAL(s, "2");
+                JSONSINK_ADD_LITERAL_KEY(s, "id");
+                jsonsink_add_uint32(s, i);
+                JSONSINK_ADD_LITERAL_KEY(s, "double");
+                jsonsink_add_double(s, -1.2345);
+                JSONSINK_ADD_LITERAL_KEY(s, "int32");
+                jsonsink_add_int32(s, -54321);
+                JSONSINK_ADD_LITERAL_KEY(s, "jsonsink_add_escaped_string");
+                jsonsink_add_escaped_string(s, JSONSINK_LITERAL("foo"));
+                JSONSINK_ADD_LITERAL_KEY(s, "jsonsink_add_serialized_value");
+                jsonsink_add_serialized_value(s,
                                               JSONSINK_LITERAL_QUOTE("foo"));
-                JSONSINK_ADD_LITERAL_KEY(&s, "null");
-                jsonsink_add_null(&s);
-                JSONSINK_ADD_LITERAL_KEY(&s, "true");
-                jsonsink_add_bool(&s, true);
-                JSONSINK_ADD_LITERAL_KEY(&s, "false");
-                jsonsink_add_bool(&s, false);
-                jsonsink_object_end(&s);
+                JSONSINK_ADD_LITERAL_KEY(s, "null");
+                jsonsink_add_null(s);
+                JSONSINK_ADD_LITERAL_KEY(s, "true");
+                jsonsink_add_bool(s, true);
+                JSONSINK_ADD_LITERAL_KEY(s, "false");
+                jsonsink_add_bool(s, false);
+                jsonsink_object_end(s);
         }
-        jsonsink_array_end(&s);
-        jsonsink_object_end(&s);
-        jsonsink_object_end(&s);
-        jsonsink_flush(&s, 0);
-        int error = jsonsink_error(&s);
+        jsonsink_array_end(s);
+        jsonsink_object_end(s);
+        jsonsink_object_end(s);
+        jsonsink_flush(s, 0);
+        int error = jsonsink_error(s);
         if (error != 0) {
                 printf("jsonsink error: %d\n", error);
                 return 1;
