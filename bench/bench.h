68bfccadc6ed5bb8b8f4135a8ba4e36946090c0e
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-03-02 21:20:15 +0900

    prepare for c++

--- bench/bench.h
+++ bench/bench.h
@@ -1,9 +1,17 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 void bench(const char *label,
            int (*fn)(unsigned int n, const double *data_double,
                      const uint32_t *data_u32));
 size_t do_fwrite(const void *p, size_t sz, size_t nitems, FILE *fp);
 
 extern bool test_run;
+
+#if defined(__cplusplus)
+}
+#endif
