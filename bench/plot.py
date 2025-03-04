e9de10ef6086323773104f47c995c0269bc106a6
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-03-04 15:41:15 +0900

    add a script to plot the result

---
+++ bench/plot.py
@@ -0,0 +1,24 @@
+import pandas
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pandas.read_csv("result.csv", header=None)
+print(df)
+df = df[::-1]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+df.plot.barh(
+    x=0,
+    y=1,
+    label="obj per second",
+    xlabel="obj per second",
+    ylabel="",
+    ax=ax1,
+)
+
+plt.title("json generator benchmark")
+plt.tight_layout()
+plt.savefig("result.png")
