cf3487b423cc63447e6ffd373263a1fb776289f3
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-03-05 09:59:27 +0900

    plot heap usage as well

--- bench/plot.py
+++ bench/plot.py
@@ -9,7 +9,7 @@ df = df[::-1]
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1, 1, 1)
-ax1.tick_params(axis='x', labelrotation=90)
+ax2 = ax1.twiny()
 
 df.plot.barh(
     x=0,
@@ -19,6 +19,22 @@ df.plot.barh(
     ylabel="",
     ax=ax1,
 )
+ax1.tick_params(axis='x', labelrotation=90)
+
+df.plot.barh(
+    x=0,
+    y=2,
+    label="peak heap usage",
+    color="#a98d1980",
+    position=1,
+    xlabel="byte",
+    ax=ax2,
+)
+
+lines1, labels1 = ax1.get_legend_handles_labels()
+lines2, labels2 = ax2.get_legend_handles_labels()
+ax1.legend().remove()
+ax2.legend(lines1 + lines2, labels1 + labels2, loc="upper right")
 
 plt.title("json generator benchmark")
 plt.tight_layout()
