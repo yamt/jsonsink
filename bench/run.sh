8ab17bc3139e7dcd5d99ca153a34b939d5a6aa5d
YAMAMOTO Takashi <imuwoto@gmail.com>
2025-05-20 18:22:33 +0900

    bench/run.sh: prefer system openssl

--- bench/run.sh
+++ bench/run.sh
@@ -1,10 +1,22 @@
 TESTS="jsonsink jsonsink-jnum jsonsink-fpconv snprintf ljson ljson_dom rapidjson cjson parson"
 
+# note: macOS's system openssl seems to have a bit differnt output format.
+# it's preferred for this script.
+if type /usr/bin/openssl; then
+dosha256() {
+	/usr/bin/openssl sha256 $1
+}
+else
+dosha256() {
+	openssl sha256 $1
+}
+fi
+
 # quick test
 printf "%13.13s:0b4864bde11d7f8893994605eca96bfc928337d3b04f1a08fcf8a515ca08d4ac\n" expected >&2
 for t in $TESTS; do
 	printf "%15.15s:" $t >&2
-	./$t --test | python -m json.tool | openssl sha256 >&2
+	./$t --test | python -m json.tool | dosha256 >&2
 done
 
 # convert the flatbuffers output to json so that it's comparable with others
@@ -12,7 +24,7 @@ TMP=$(mktemp -d)
 ./flatbuffers --test > $TMP/test.bin
 flatc --raw-binary --json --strict-json test.fbs -- $TMP/test.bin
 printf "%15.15s:" flatbuffers >&2
-python -m json.tool test.json | openssl sha256 >& 2
+python -m json.tool test.json | dosha256 >& 2
 
 for t in $TESTS; do
 	printf "%15.15s:" $t >&2
